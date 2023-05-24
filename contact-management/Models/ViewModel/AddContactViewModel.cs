527b744d9a23f60de2d9810b11ab2abaaf038e36
FernandoPrado <dev.fprado@gmail.com>
2023-05-24 12:11:33 -0700

    Added feature to add a new contact

---
+++ contact-management/Models/ViewModel/AddContactViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace contact_management.Models.ViewModel
+{
+    public class AddContactViewModel
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
